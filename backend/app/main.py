2bd963aaaec57ad4873682b1207d7fd72ecc8905
suelmaparis <iscac11802@alumni.iscac.pt>
2026-09-17 14:49:21 -0400

    upate

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,9 +21,7 @@ from app.api.monitoring import (
 )
 
 from app.core.init_db import init_db
-from slowapi import (
-    _rate_limit_exceeded_handler,
-)
+
 from slowapi.errors import RateLimitExceeded
 
 init_db()
